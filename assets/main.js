d2934f923ec7eb425e1facca59b7063b6c6ae2d2
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-23 14:01:15 +0300

    Assignemt02 -Counter

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,42 @@
-const button5 = document.querySelector("#plusFive")
-const indicator = document.querySelector("#number-indicator")
-const disp = document.querySelector("#display-heading")
+const button5 = document.querySelector("#plusFive");
+const button10 = document.querySelector("#plusTen");
+const button15 = document.querySelector("#plusFifteen");
+const button20 = document.querySelector("#plusTwenty");
 
-button5.addEventListener('click', () => {
-  disp.textContent = "added 5"
-  indicator.textContent = (parseInt(indicator.textContent) + 5).toString()
-})
+const indicator = document.querySelector("#number-indicator");
+const disp = document.querySelector("#display-heading");
+
+const button = document.querySelectorAll("button");
+
+button.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const value = btn.getAttribute("value");
+    disp.textContent = `added ${value}`;
+    indicator.textContent = (
+      parseInt(indicator.textContent) + parseInt(value)
+    ).toString();
+  });
+});
+
+// each button event listener
+
+// button5.addEventListener("click", () => {
+//   disp.textContent = "added 5";
+//   indicator.textContent = (parseInt(indicator.textContent) + 5).toString();
+// });
+
+// button10.addEventListener("click", () => {
+//   disp.textContent = "added 10";
+//   indicator.textContent = (parseInt(indicator.textContent) + 10).toString();
+// });
+
+// button15.addEventListener("click", () => {
+//   disp.textContent = "added 15";
+//   indicator.textContent = (parseInt(indicator.textContent) + 15).toString();
+// });
+
+// button20.addEventListener("click", () => {
+//   disp.textContent = "added 20";
+//   indicator.textContent = (parseInt(indicator.textContent) + 20).toString();
+// });
